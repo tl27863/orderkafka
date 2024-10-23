e9cc746788f7236815da8e6e86717f3fb845caf8
Tommy Lukmana <tommy.lukmana@skiff.com>
2024-10-23 17:05:52 +0700

    fixed validation in inventory route and added corresponding test

--- src/routes/inventoryRoute.ts
+++ src/routes/inventoryRoute.ts
@@ -1,33 +1,32 @@
 import express, { Request, Response } from "express";
 import { InventoryService } from "../services/inventoryService";
+import { validate as uuidValidate } from "uuid";
 
 const inventoryService = new InventoryService();
 const router = express.Router();
+
+function validateUUID(uuid: string): boolean {
+  return uuidValidate(uuid);
+}
 
 router.post(
   "/update/:productId/:quantity",
   async (req: Request, res: Response) => {
     const { productId, quantity } = req.params;
-    const qty = parseInt(quantity, 10);
+    const qty = Number(quantity);
 
-    if (isNaN(qty)) {
-      res
-        .status(400)
-        .send({ message: "Invalid quantity. Please provide a valid integer." });
+    if (!validateUUID(productId) || isNaN(qty) || !Number.isInteger(qty)) {
+      res.status(400).send({ message: "Invalid input" });
+    } else {
+      await inventoryService.createOrUpdateInventory(productId, qty);
+      res.status(200).send({ message: "Inventory updated" });
     }
-
-    if (!Number.isInteger(qty)) {
-      res.status(400).send({ message: "Quantity must be a whole number." });
-    }
-
-    await inventoryService.createOrUpdateInventory(productId, qty);
-    res.status(200).send({ message: "Inventory updated" });
   },
 );
 
 router.get("/stock", async (req: Request, res: Response) => {
   const { productId } = req.query;
-  if (typeof productId !== "string" || !productId) {
+  if (typeof productId !== "string" || !productId || !validateUUID(productId)) {
     res.status(400).send({ message: "Invalid Id" });
   } else {
     const result = await inventoryService.getInventory(productId);
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -13,18 +13,46 @@ afterAll(async () => {
 });
 
 describe("POST & GET /api/inventory", () => {
-  it("should successfuly input data to db and output it", async () => {
+  it("Successfuly input data to db and output it", async () => {
     await deleteAllData();
+
     const uuid = crypto.randomUUID();
     const postRes = await request(app)
       .post(`/api/inventory/update/${uuid}/1`)
       .set("Accept", "application/json");
+
     const getRes = await request(app)
       .get(`/api/inventory/stock?productId=${uuid}`)
       .set("Accept", "application/json");
+
     expect(postRes.statusCode).toBe(200);
     expect(getRes.statusCode).toBe(200);
     expect(getRes.body.product_id).toBe(uuid);
     expect(getRes.body.quantity).toBe(1);
+  });
+  it("Successfuly response to invalid data", async () => {
+    await deleteAllData();
+
+    const uuid = crypto.randomUUID();
+    const postNanRes = await request(app)
+      .post(`/api/inventory/update/${uuid}/notanumber`)
+      .set("Accept", "application/json");
+
+    const postDecimalRes = await request(app)
+      .post(`/api/inventory/update/${uuid}/3.14`)
+      .set("Accept", "application/json");
+
+    const postUUIDRes = await request(app)
+      .post(`/api/inventory/update/notauuid/37`)
+      .set("Accept", "application/json");
+
+    const getUUIDRes = await request(app)
+      .get(`/api/inventory/stock?productId=notauuid`)
+      .set("Accept", "application/json");
+
+    expect(postNanRes.statusCode).toBe(400);
+    expect(postDecimalRes.statusCode).toBe(400);
+    expect(postUUIDRes.statusCode).toBe(400);
+    expect(getUUIDRes.statusCode).toBe(400);
   });
 });
